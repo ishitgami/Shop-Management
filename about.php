9bb6c7740e40e8ad901a088f1406fcd13b523da4
unknown <ishitgami01@gmail.com>
2021-09-22 15:23:32 +0530

    create abot page

---
+++ about.php
@@ -0,0 +1,34 @@
+<html>
+    <head>
+        <title>About</title>
+        <style>
+            *{
+                margin: 0;
+                padding: 0;
+                box-sizing: border-box;
+            }
+            .about-head {
+                margin: 20px;
+            }
+            .createdBy{
+                margin: 20px;
+            }
+        </style>
+    </head>
+    <body>
+    <?php require 'header.php'; ?>
+    <div class="about-head">
+    <h1>
+        SHOP MANAGEMENT
+    </h1>
+    <p>
+        Shop Management Is the managemnt of customer in shop.You can add customer ,delete customer and update customer information.
+
+    </p>
+    </div>
+    <div class="createdBy">
+        <h2>Created By</h2>
+        <h3>Ishit Gami, Dhrupal Rafaliya, Urvish Buha, Jaimil Dabhi</h3>
+    </div>
+    </body>
+</html>
